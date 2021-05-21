7ed7a1242f645ce58745df405236bcd85a21df40
tarcea <tarcea@gmail.com>
2021-05-21 10:09:27 +0200

    demo account added on about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,10 +15,13 @@ const About = () => {
     <div className={styles.container}>
       <h1 onClick={navigateTo("home")}>My cool bookmark</h1>
         <div className={styles.text}>
-        <h5>The app</h5>
+        <h3>The app</h3>
           <p>This is a web application developed using Firebase (authetication, firestore, hosting), React (hooks, router, context, css.module) and plain CSS. </p>
+          <p><strong>Demo account:</strong></p>
+          <small>email: test210@test210.com</small><br />
+          <small>password: 123456</small><br /><br />
           <button onClick={navigateTo("login")}>Get Started</button>
-          <h5>User journey</h5>
+          <h3>User journey</h3>
           <p>When you navigate at <span onClick={navigateTo("home")}><strong>https://mycoolbookmark.web.app/home</strong></span>, you can see the page with all public bookmarks from all users. Here you just can search bookmarks and visit the web pages.
           You can create your own profile and add bookmarks if you sign up by clicking on the "Login" button and then, at the bottom of the page click on "Sign Up".
           If you are logged in, you can create public or private bookmarks by pasting your preferred web addresses in the dedicated field and pressing "preview".
