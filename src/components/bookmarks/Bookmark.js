2055dd9c1842947d503738341873c500104ad466
tarcea <tarcea@gmail.com>
2021-06-25 21:41:07 +0200

    placeholder for missing photos added

--- src/components/bookmarks/Bookmark.js
+++ src/components/bookmarks/Bookmark.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { db } from '../../firebase';
 import styles from './bookmarks.module.css';
-const placeholder = "https://cdn.dribbble.com/users/119159/screenshots/5718165/jazz-pattern-dribbble.jpg?compress=1&resize=800x600";
 
 const Bookmark = ({ bookmark, currentUser }) => {
-
+  const randomPicture = () => {
+    const randomNo = Math.floor(Math.random() * 9)
+    const placeholder = `https://picsum.photos/3${randomNo}${randomNo}/15${randomNo}`;
+    return placeholder;
+  };
   const deleteBookmark = (e) => {
     e.stopPropagation();
     const confirmDelete = window.confirm(`Are you sure you want to delete "${bookmark.title}" bookmark??`);
@@ -23,7 +26,7 @@ const Bookmark = ({ bookmark, currentUser }) => {
     <div className={styles.box} onClick={() => window.open(bookmark.url, "_blank")}>
       <h5><strong>{bookmark.title}</strong></h5>
       <div className={styles.img} >
-        <img src={bookmark.image || placeholder} alt={bookmark.title} />
+        <img src={bookmark.image || randomPicture()} alt={bookmark.title} />
       </div>
       <div className={styles.description}>
         <p>{bookmark.description}</p>
