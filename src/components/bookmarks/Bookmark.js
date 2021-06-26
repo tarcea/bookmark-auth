2447c00489e489b1d5c66cf4ed675ad89a8ba8c4
tarcea <tarcea@gmail.com>
2021-06-26 12:50:22 +0200

    placeholder for missing photos added

--- src/components/bookmarks/Bookmark.js
+++ src/components/bookmarks/Bookmark.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { db } from '../../firebase';
 import styles from './bookmarks.module.css';
+const placeholder = "https://firebasestorage.googleapis.com/v0/b/list-101.appspot.com/o/placeholder.jpg?alt=media&token=72924b11-5bf4-4ecd-a793-a6bd329acdcd";
 
 const Bookmark = ({ bookmark, currentUser }) => {
-  const randomPicture = () => {
-    const randomNo = Math.floor(Math.random() * 9)
-    const placeholder = `https://picsum.photos/3${randomNo}${randomNo}/15${randomNo}`;
-    return placeholder;
-  };
+  // const randomPicture = () => {
+  //   const randomNo = Math.floor(Math.random() * 9)
+  //   const placeholder = `https://picsum.photos/3${randomNo}${randomNo}/15${randomNo}`;
+  //   return placeholder;
+  // };
   const deleteBookmark = (e) => {
     e.stopPropagation();
     const confirmDelete = window.confirm(`Are you sure you want to delete "${bookmark.title}" bookmark??`);
@@ -26,7 +27,7 @@ const Bookmark = ({ bookmark, currentUser }) => {
     <div className={styles.box} onClick={() => window.open(bookmark.url, "_blank")}>
       <h5><strong>{bookmark.title}</strong></h5>
       <div className={styles.img} >
-        <img src={bookmark.image || randomPicture()} alt={bookmark.title} />
+        <img src={bookmark.image || placeholder} alt={bookmark.title} />
       </div>
       <div className={styles.description}>
         <p>{bookmark.description}</p>
--- src/components/bookmarks/LinkPreview.js
+++ src/components/bookmarks/LinkPreview.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import styles from './bookmarks.module.css';
+const placeholder = "https://firebasestorage.googleapis.com/v0/b/list-101.appspot.com/o/placeholder.jpg?alt=media&token=72924b11-5bf4-4ecd-a793-a6bd329acdcd";
+
 
 const LinkPreview = (props) => {
-  const { data, setUrl, setPreview, createNew, isPublic, setIsPublic } =props;
+  const { data, setUrl, setPreview, createNew, isPublic, setIsPublic } = props;
 
   const cancelPreview = () => {
     setPreview(false);
@@ -14,26 +16,26 @@ const LinkPreview = (props) => {
   };
 
   return (
-    <div style={{textAlign: 'center'}}>
+    <div style={{ textAlign: 'center' }}>
       <label>public</label>
-      <input type="checkbox" name="public" checked={isPublic} onChange={onInputChange} style={{width: "20px"}}/>
-        <button 
-          className={styles.buttonGhost} 
-          onClick={cancelPreview}
-        >
-          cancel
-        </button>
-        <button 
-          className={styles.buttonGhost}  
-          onClick={createNew}
-        >
-          bookmark
-        </button>
-      <p>Domain: { data.url }</p>
-      <p>Title: { data.title }</p>
-      <p>Description: { data.description }</p>
-      <img height="100px" width="100px" src={data.image} alt={data.title} />
-      </div>
+      <input type="checkbox" name="public" checked={isPublic} onChange={onInputChange} style={{ width: "20px" }} />
+      <button
+        className={styles.buttonGhost}
+        onClick={cancelPreview}
+      >
+        cancel
+      </button>
+      <button
+        className={styles.buttonGhost}
+        onClick={createNew}
+      >
+        bookmark
+      </button>
+      <p>Domain: {data.url}</p>
+      <p>Title: {data.title}</p>
+      <p>Description: {data.description}</p>
+      <img height="100px" width="100px" src={data.image || placeholder} alt={data.title} />
+    </div>
   );
 };
 
