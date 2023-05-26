9db43d8d601b6291c11b80a5c61c7c610bd5a190
tarcea <tarcea@gmail.com>
2023-05-26 09:37:23 +0200

    delete button styled

--- src/components/bookmarks/Bookmark.js
+++ src/components/bookmarks/Bookmark.js
@@ -1,43 +1,43 @@
 import React from 'react';
 import { db } from '../../firebase';
 import styles from './bookmarks.module.css';
-const placeholder = "https://firebasestorage.googleapis.com/v0/b/list-101.appspot.com/o/placeholder.jpg?alt=media&token=72924b11-5bf4-4ecd-a793-a6bd329acdcd";
+const placeholder =
+  'https://firebasestorage.googleapis.com/v0/b/list-101.appspot.com/o/placeholder.jpg?alt=media&token=72924b11-5bf4-4ecd-a793-a6bd329acdcd';
 
 const Bookmark = ({ bookmark, currentUser }) => {
-  // const randomPicture = () => {
-  //   const randomNo = Math.floor(Math.random() * 9)
-  //   const placeholder = `https://picsum.photos/3${randomNo}${randomNo}/15${randomNo}`;
-  //   return placeholder;
-  // };
   const deleteBookmark = (e) => {
     e.stopPropagation();
-    const confirmDelete = window.confirm(`Are you sure you want to delete "${bookmark.title}" bookmark??`);
+    const confirmDelete = window.confirm(
+      `Are you sure you want to delete "${bookmark.title}" bookmark??`
+    );
     if (!confirmDelete) {
-      return
+      return;
     }
     db.collection('bookmarks').doc(bookmark.id).delete();
   };
 
-  const editBookmark = (e) => {
-    e.stopPropagation();
-    alert(`edit '${bookmark.title}' ???`)
-  };
+  // const editBookmark = (e) => {
+  //   e.stopPropagation();
+  //   alert(`edit '${bookmark.title}' ???`);
+  // };
 
   return (
-    <div className={styles.box} onClick={() => window.open(bookmark.url, "_blank")}>
-      <h5><strong>{bookmark.title}</strong></h5>
-      <div className={styles.img} >
+    <div
+      className={styles.box}
+      onClick={() => window.open(bookmark.url, '_blank')}
+    >
+      <h5>
+        <strong>{bookmark.title}</strong>
+      </h5>
+      <div className={styles.img}>
         <img src={bookmark.image || placeholder} alt={bookmark.title} />
       </div>
       <div className={styles.description}>
         <p>{bookmark.description}</p>
       </div>
-      {currentUser && (currentUser.uid === bookmark.userId) && (
+      {currentUser && currentUser.uid === bookmark.userId && (
         <>
-          <button
-            className={styles.buttonX}
-            onClick={deleteBookmark}
-          >
+          <button className={styles.buttonX} onClick={deleteBookmark}>
             X
           </button>
           {/* <button
@@ -47,8 +47,7 @@ const Bookmark = ({ bookmark, currentUser }) => {
             edit
           </button> */}
         </>
-      )
-      }
+      )}
     </div>
   );
 };
